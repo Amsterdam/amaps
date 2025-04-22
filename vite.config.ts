d89e2928af9eb4e2d409d0257097cd680fe89439
ferhany2002 <ferhan.y2002@gmail.com>
2025-04-22 12:42:22 +0200

    Add polling for hot reload

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), tsconfigPaths()],
   root: "./app",
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
