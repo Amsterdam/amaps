a9d822f541a45060247b62a5a9414b8cc5616aea
ferhany2002 <ferhan.y2002@gmail.com>
2025-04-30 15:13:19 +0200

    Add algemeen option to legend

--- app/types/parkingTypes.ts
+++ app/types/parkingTypes.ts
@@ -1,10 +1,11 @@
 export const parkingTypes: Record<string, { label: string; color: string }> = {
   E1: { label: "Parkeerverbod", color: "#f47b7b" },
   E5: { label: "Taxistandplaats", color: "#FFFF00" },
-  E6a: { label: "Gehandicaptenparkeerplaats algemeen", color: "#80cef3" },
-  E6b: { label: "Gehandicaptenparkeerplaats kenteken", color: "#80cef3" },
+  E6a: { label: "Algemeen gehandicaptenparkeerplaats", color: "#80cef3" },
+  E6b: { label: "Kenteken gehandicaptenparkeerplaats", color: "#80cef3" },
   E7: { label: "Laden en lossen", color: "#d5c0ad" },
   E8: { label: "Specifieke voertuigcategorie", color: "#f17fc0" },
   E9: { label: "Vergunninghouders", color: "#ca70b3" },
   E10: { label: "Blauwe zone", color: "#7ea1cb" },
+  "": {label: "Zonder parkeertype", color: "#3388ff" },
 };
