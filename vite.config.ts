f2434d81a146912b2b1175aa3c6226b9410e82c9
ferhany2002 <ferhan.y2002@gmail.com>
2025-05-01 13:22:50 +0200

    Revert vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), tsconfigPaths()],
-  root: "./app",
-  build: {
-    lib: {
-      entry: path.resolve(
-        __dirname,
-        "app/components/MultiSelect/EmbeddedMultiSelect.tsx"
-      ),
-      name: "MultiSelectMap",
-      fileName: () => "multiselect.iife.js",
-      formats: ["iife"],
-    },
-    outDir: "dist-embed",
-  },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify("production"),
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
+  root: path.join(__dirname, "app"),
 });
