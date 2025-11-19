7be6262c1033944009ea5cb834e56bdd1bb173e4
ferhany <f.yildiz2002@gmail.com>
2025-11-19 09:22:02 +0100

    Inject the env var into the iife.js

--- env.sh
+++ env.sh
@@ -13,3 +13,13 @@ echo "  AMSTERDAM_API_KEY: \"${AMSTERDAM_API_KEY}\"" >> $CONFIG_FILE
 echo "}" >> $CONFIG_FILE
 
 echo "Runtime environment file created at $CONFIG_FILE"
+
+# Inject into the embedded IIFE bundle
+EMBED_FILE="/usr/share/nginx/html/multiselect.iife.js"
+
+if [ -f "$EMBED_FILE" ]; then
+  echo "Injecting AMSTERDAM_API_KEY into multiselect.iife.js"
+  
+  # Append a line with the runtime env var
+  sed -i "1s;^;window.AMSTERDAM_API_KEY=\"${AMSTERDAM_API_KEY}\";\n;" "$EMBED_FILE"
+fi
