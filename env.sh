4ae63d7eafca4da67dd604ab4dbb78caa22d81f9
ferhany <f.yildiz2002@gmail.com>
2025-11-19 10:25:29 +0100

    Revert changes

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WWW_DIR=/app/www
-CONFIG_FILE=$WWW_DIR/env-config.js
+FOLDER=/var/www/html/env-config
+CONFIG_FILE=$FOLDER/env-config.js
 
-# Recreate env-config.js
+# Recreate file
 rm -f $CONFIG_FILE
 touch $CONFIG_FILE
 
@@ -15,13 +15,12 @@ echo "}" >> $CONFIG_FILE
 echo "Runtime environment file created at $CONFIG_FILE"
 
 # Inject into the embedded IIFE bundle
-EMBED_FILE="$WWW_DIR/multiselect.iife.js"
+EMBED_FILE="/usr/share/nginx/html/multiselect.iife.js"
 
 if [ -f "$EMBED_FILE" ]; then
   echo "Injecting AMSTERDAM_API_KEY into multiselect.iife.js"
   
   # Prepend a line with the runtime env var
   sed -i "1s|^|window.AMSTERDAM_API_KEY=\"${AMSTERDAM_API_KEY}\";\n|" "$EMBED_FILE"
-else
-  echo "Embedded IIFE not found at $EMBED_FILE, skipping injection"
+
 fi
